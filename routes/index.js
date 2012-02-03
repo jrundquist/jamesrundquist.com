f72b91de5d0d217018ce3f71a3b6f718bc0d57fb
James Rundquist <jrscienceguy@gmail.com>
2012-02-03 00:11:49 -0500

    Added email contact form. Fixed styling for projects on resize. Also some spelling errors corrected on resume

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,27 @@
 exports.index = function(req, res){
   res.render('index', { title: 'James Rundquist' })
 };
+
+exports.sendEmail = function(req, res){
+  
+  var email = require('mailer');
+  email.send({
+    host : "localhost",              // smtp server hostname
+    port : "25",                     // smtp server port
+    domain : "localhost",            // domain used by client to identify itself to server
+    to : "james.k.rundquist@gmail.com",
+    from : "contact@jamesrundquist.com",
+    subject : "Contact Email from: "+req.body.contact.n,
+    template : "views/email/contact.txt",   // path to template name
+    data : {
+      "message": req.body.contact.m,
+      "name": req.body.contact.n,
+      "email": req.body.contact.e
+    }
+  },
+  function(err, result){
+    if(err){ console.log('Error:', err, req.body, result); }
+  });
+  res.render('include/response',{layout:false,data:'Thanks! I will get back to your shortly!'});
+}
+  
